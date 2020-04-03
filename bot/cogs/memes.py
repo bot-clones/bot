65ec52e3baa70cd1463acaf75c8f15f507c1b397
hd4niel <daniel.holmberg97@gmail.com>
2020-04-03 18:25:49 +0300

    Improve meme cog

--- bot/cogs/memes.py
+++ bot/cogs/memes.py
@@ -10,82 +10,54 @@ class Memes(commands.Cog):
         self.bot = bot
         self._last_member = None
 
+    async def send_meme(self, ctx, meme):
+        url = f'https://meme-api.herokuapp.com/gimme{meme}'
+        page = requests.get(url)
+        soup = BeautifulSoup(page.content, 'html.parser')
+        site_json=json.loads(soup.text)
+        e = discord.Embed()
+        e.set_image(url=site_json['url'])
+        await ctx.send(embed=e)
+
     @commands.command(name='meme', help='Random')
     async def _meme(self, ctx):
         await ctx.message.add_reaction('🐸')
-        url = 'https://meme-api.herokuapp.com/gimme'
-        page = requests.get(url)
-        soup = BeautifulSoup(page.content, 'html.parser')
-        site_json=json.loads(soup.text)
-        e = discord.Embed()
-        e.set_image(url=site_json['url'])
-        await ctx.send(embed=e)
+        await self.send_meme(ctx, "")
 
-    @commands.command(name='fyssa', help='pi=3')
+    @commands.command(name='fyssa', aliases=['physics', 'fysik'], help='pi=3')
     async def _physics(self, ctx):
         await ctx.message.add_reaction('🖖')
-        url = 'https://meme-api.herokuapp.com/gimme/physicsmemes'
-        page = requests.get(url)
-        soup = BeautifulSoup(page.content, 'html.parser')
-        site_json=json.loads(soup.text)
-        e = discord.Embed()
-        e.set_image(url=site_json['url'])
-        await ctx.send(embed=e)
+        await self.send_meme(ctx, "/physicsmemes")
 
-    @commands.command(name='mafs', help='1+2+3+...=-1/12')
+    @commands.command(name='mafs', aliases=['math', 'matte'], help='1+2+3+...=-1/12')
     async def _maths(self, ctx):
         await ctx.message.add_reaction('➗')
-        url = 'https://meme-api.herokuapp.com/gimme/mathmemes'
-        page = requests.get(url)
-        soup = BeautifulSoup(page.content, 'html.parser')
-        site_json=json.loads(soup.text)
-        e = discord.Embed()
-        e.set_image(url=site_json['url'])
-        await ctx.send(embed=e)
+        await self.send_meme(ctx, "/mathmemes")
 
     @commands.command(name='dank', help='420')
     async def _dank(self, ctx):
         await ctx.message.add_reaction('🆘')
-        url = 'https://meme-api.herokuapp.com/gimme/dankmemes'
-        page = requests.get(url)
-        soup = BeautifulSoup(page.content, 'html.parser')
-        site_json=json.loads(soup.text)
-        e = discord.Embed()
-        e.set_image(url=site_json['url'])
-        await ctx.send(embed=e)
+        await self.send_meme(ctx, "/dankmemes")
 
-    @commands.command(name='gegga', help='60°10\'15.2\"N 24°57\'22.6\"E')
+    @commands.command(name='gegga', aliases=['geography', 'geografi'], help='60°10\'15.2\"N 24°57\'22.6\"E')
     async def _geography(self, ctx):
         await ctx.message.add_reaction('🌍')
-        url = 'https://meme-api.herokuapp.com/gimme/geographymemes'
-        page = requests.get(url)
-        soup = BeautifulSoup(page.content, 'html.parser')
-        site_json=json.loads(soup.text)
-        e = discord.Embed()
-        e.set_image(url=site_json['url'])
-        await ctx.send(embed=e)
+        await self.send_meme(ctx, "/geographymemes")
 
-    @commands.command(name='cs', help='01100011 01110011')
+    @commands.command(name='data', aliases=['cs'], help='01100011 01110011')
     async def _cs(self, ctx):
         await ctx.message.add_reaction('👩‍💻')
-        url = 'https://meme-api.herokuapp.com/gimme/ProgrammerHumor'
-        page = requests.get(url)
-        soup = BeautifulSoup(page.content, 'html.parser')
-        site_json=json.loads(soup.text)
-        e = discord.Embed()
-        e.set_image(url=site_json['url'])
-        await ctx.send(embed=e)
+        await self.send_meme(ctx, "/ProgrammerHumor")
 
-    @commands.command(name='kemist', help='H2O + H2O = H4O')
+    @commands.command(name='kemma', aliases=['chemistry', 'kemi'], help='H2O+H2O=H4O')
     async def _chemistry(self, ctx):
         await ctx.message.add_reaction('🧪')
-        url = 'https://meme-api.herokuapp.com/gimme/chemistrymemes'
-        page = requests.get(url)
-        soup = BeautifulSoup(page.content, 'html.parser')
-        site_json=json.loads(soup.text)
-        e = discord.Embed()
-        e.set_image(url=site_json['url'])
-        await ctx.send(embed=e)
+        await self.send_meme(ctx, "/chemistrymemes")
+
+    @commands.command(name='stenar', aliases=['geology', 'geologi'], help='Geologi såklart')
+    async def _chemistry(self, ctx):
+        await ctx.message.add_reaction('🗿')
+        await self.send_meme(ctx, "/Geologymemes")
 
 def setup(bot):
     bot.add_cog(Memes(bot))
